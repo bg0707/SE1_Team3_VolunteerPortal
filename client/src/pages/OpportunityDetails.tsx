31de136f7cb41dcaa5f9d59f45b935b72314fc0c
CunBr <gasparbruno@icloud.com>
2025-11-27 20:42:46 +0100

    Backend to view Opportunity Details.

---
+++ client/src/pages/OpportunityDetails.tsx
@@ -0,0 +1,37 @@
+// OpportunityDetailsPage
+
+// Here, you are going to display the differnt details about a specific Opportunity. 
+
+// 1. Read teh ID from the URL
+
+// 2. Fetch teh opportunity using the function in the api (opportunity.api.ts)
+
+// 3. Display information 
+
+// Example of what is returned by the function in the api (opportunity.api.ts)
+
+/*
+{
+"opportunityId":4,
+"organizationId":2,
+"categoryId":1,
+"title":"Park Cleanup Volunteer",
+"description":"Help clean the central park.",
+"location":"Luxembourg",
+"date":"2025-05-12T00:00:00.000Z",
+"createdAt":"2025-11-22T21:03:01.000Z",
+"category": {
+    "categoryId":1,
+    "name":"Environment"
+    },
+"organization":{
+    "organizationId":2,
+    "userId":1,
+    "name":"Helping Hands",
+    "description":"Test organization",
+    "createdAt":"2025-11-22T21:01:20.000Z"
+    }
+}
+
+
+*/
