2f3dc4f085f089aa2e49fbc3934ee2cd6018178b
Cedric29-05 <cedric.bassong.001@student.uni.lu>
2025-11-29 16:29:25 +0100

    Progress in OpportunityDeatails

--- client/src/pages/OpportunityDetails.tsx
+++ client/src/pages/OpportunityDetails.tsx
@@ -1,12 +1,44 @@
+import { useEffect, useState } from "react";
+import { fetchOpportunityById } from "../api/opportunity.api";
+import type { Opportunity } from "../components/OpportunityCard";
+import { useParams } from "react-router-dom";
 // OpportunityDetailsPage
 
 // Here, you are going to display the differnt details about a specific Opportunity. 
 
-// 1. Read teh ID from the URL
+
+
+// 1. Read the ID from the URL
 
 // 2. Fetch teh opportunity using the function in the api (opportunity.api.ts)
 
 // 3. Display information 
+
+export default function OpportunityDetails() {
+    const [opportunity, setOpportunity] = useState<Opportunity | null>(null);
+    const { id } = useParams<{ id: string }>(); // 1. Read the ID from the URL
+    
+    useEffect(() => {
+        if (id) {
+            fetchOpportunityById(parseInt(id)).then(setOpportunity);
+        }
+    }, [id]); 
+
+     // 3. Display information
+    if (!opportunity) return <div>Loading...</div>;
+
+    return (
+        <div className="max-w-screen-xl mx-auto mt-28 p-4">
+            <h1>{opportunity.title}</h1>
+            <p>{opportunity.description}</p>
+            <p>Location: {opportunity.location}</p>
+            <p>Date: {opportunity.date}</p>
+            {/* etc. */}
+        </div>
+    );
+}
+
+
 
 // Example of what is returned by the function in the api (opportunity.api.ts)
 
